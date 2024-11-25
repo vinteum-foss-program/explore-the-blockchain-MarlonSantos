cae2d8a423ea19d04caaf4c4ce7eae66a385ef2c
MarlonSantos <marlonnit@gmail.com>
2024-11-25 09:50:28 -0300

    01 to 04 done.

--- submission/001.sh
+++ submission/001.sh
@@ -1,1 +1,25 @@
+#!/bin/bash
+
 # What is the hash of block 654,321?
+
+getBlockHash() {
+  local blockNumber=$1
+  local blockHash
+  blockHash=$(bitcoin-cli getblockhash "$blockNumber" 2>/dev/null)
+  if [ $? -eq 0 ]; then
+    echo "    O hash do bloco $blockNumber é: $blockHash"
+  else
+    echo "    Erro: Não foi possível obter o hash do bloco $blockNumber."
+  fi
+}
+
+echo "Questão 01:"
+echo ""
+echo "  What is the hash of block 654,321?"
+echo ""
+echo "  Resposta:"
+echo ""
+blockNumber=654321
+getBlockHash "$blockNumber"
+
+
